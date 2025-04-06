bbb28d96d6dd95a96cbb72ff0bdf926990d7d16f
Nevskiy911 <maleev1820@gmail.com>
2025-04-06 22:13:53 +0200

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,18 @@
 import s from "./Contact.module.css";
+import { FaPhone, FaUser } from "react-icons/fa";
 
 function Contact({ contact, onDelete }) {
   return (
     <li className={s.item}>
       <div className={s.contacts}>
-        <p>{contact.name}</p>
-        <p>{contact.number}</p>
+        <p>
+          <FaUser style={{ marginRight: "8px" }} />
+          {contact.name}
+        </p>
+        <p>
+          <FaPhone style={{ marginRight: "8px", transform: "scaleX(-1)" }} />
+          {contact.number}
+        </p>
       </div>
       <button className={s.delete} onClick={onDelete}>
         Delete
